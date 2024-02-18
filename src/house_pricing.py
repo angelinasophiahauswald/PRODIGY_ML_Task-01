610b76a022a63c2a338a8f57c3059f0fde27c813
Angelina <ashauswald@gmail.com>
2024-02-18 18:33:06 +0100

    implement multivariate regression model

--- src/house_pricing.py
+++ src/house_pricing.py
@@ -1,9 +1,24 @@
 import sys
 import pandas as pd
+import numpy as np
 
 def read_data(data):
     df = pd.read_csv(data)
     return df
+
+def multivariate_regression(x, w, b): 
+    """
+    single predict using linear regression
+    Args:
+      x (ndarray): Shape (n,) example with multiple features
+      w (ndarray): Shape (n,) model parameters   
+      b (scalar):             model parameter 
+      
+    Returns:
+      p (scalar):  prediction
+    """
+    p = np.dot(x, w) + b     
+    return p   
 
 if __name__ == "__main__":
     data = sys.argv[1]
