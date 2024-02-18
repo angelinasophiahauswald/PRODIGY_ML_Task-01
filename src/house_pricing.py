d1b6b156cf9e2d667a22bb111c2da4fa0a24463a
Angelina <ashauswald@gmail.com>
2024-02-18 18:51:15 +0100

    implement cost function

--- src/house_pricing.py
+++ src/house_pricing.py
@@ -7,20 +7,32 @@ def read_data(data):
     return df
 
 def multivariate_regression(x, w, b): 
+    reg = np.dot(x, w) + b     
+    return reg 
+
+def cost_function(X, y, w, b): 
     """
-    single predict using linear regression
+    compute cost
     Args:
-      x (ndarray): Shape (n,) example with multiple features
-      w (ndarray): Shape (n,) model parameters   
-      b (scalar):             model parameter 
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
       
     Returns:
-      p (scalar):  prediction
+      cost (scalar): cost
     """
-    p = np.dot(x, w) + b     
-    return p   
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):                                
+        f_wb_i = np.dot(X[i], w) + b           #(n,)(n,) = scalar (see np.dot)
+        cost = cost + (f_wb_i - y[i])**2       #scalar
+    cost = cost / (2 * m)                      #scalar    
+    return cost
 
 if __name__ == "__main__":
     data = sys.argv[1]
-    print(read_data(data))
+    x = np.array([1, 1, 1])
+    w = np.array([2, 2, 2])
+    print(multivariate_regression(x, w, 1))    
     
