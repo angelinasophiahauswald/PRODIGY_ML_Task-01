b7c69720b6f2b8f2630fa3539b6e49b1a531f675
Angelina <ashauswald@gmail.com>
2024-02-18 20:16:18 +0100

    implement gradient decent, select training input columns and convert them to arrays

--- src/house_pricing.py
+++ src/house_pricing.py
@@ -4,7 +4,11 @@ import numpy as np
 
 def read_data(data):
     df = pd.read_csv(data)
-    return df
+    x = df[["LotArea", "BedroomAbvGr", "FullBath"]]
+    x = np.array(x)
+    y = df["SalePrice"]
+    y = np.array(y)
+    return x, y
 
 def multivariate_regression(x, w, b): 
     reg = np.dot(x, w) + b     
@@ -30,9 +34,96 @@ def cost_function(X, y, w, b):
     cost = cost / (2 * m)                      #scalar    
     return cost
 
+def compute_gradient(X, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape           #(number of examples, number of features)
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):                             
+        err = (np.dot(X[i], w) + b) - y[i]   
+        for j in range(n):                         
+            dj_dw[j] = dj_dw[j] + err * X[i, j]    
+        dj_db = dj_db + err                        
+    dj_dw = dj_dw / m                                
+    dj_db = dj_db / m                                
+        
+    return dj_db, dj_dw
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    """
+    Performs batch gradient descent to learn w and b. Updates w and b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      X (ndarray (m,n))   : Data, m examples with n features
+      y (ndarray (m,))    : target values
+      w_in (ndarray (n,)) : initial model parameters  
+      b_in (scalar)       : initial model parameter
+      cost_function       : function to compute cost
+      gradient_function   : function to compute the gradient
+      alpha (float)       : Learning rate
+      num_iters (int)     : number of iterations to run gradient descent
+      
+    Returns:
+      w (ndarray (n,)) : Updated values of parameters 
+      b (scalar)       : Updated value of parameter 
+      """
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db,dj_dw = gradient_function(X, y, w, b)   ##None
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               ##None
+        b = b - alpha * dj_db               ##None
+      
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( cost_function(X, y, w, b))
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")
+        
+    return w, b, J_history #return final w,b and J history for graphing
+
 if __name__ == "__main__":
     data = sys.argv[1]
     x = np.array([1, 1, 1])
     w = np.array([2, 2, 2])
-    print(multivariate_regression(x, w, 1))    
-    
+    print(read_data(data))
+    print(multivariate_regression(x, w, 1))
+
+    '''# initialize parameters
+    initial_w = np.zeros_like(w_init)
+    initial_b = 0.
+    # some gradient descent settings
+    iterations = 1000
+    alpha = 5.0e-7
+    # run gradient descent 
+    w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b,
+                                                        compute_cost, compute_gradient, 
+                                                        alpha, iterations)
+    print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+    m,_ = X_train.shape
+    for i in range(m):
+        print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")  
+    '''
